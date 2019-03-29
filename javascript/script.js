ca097c18cdefa87a1e3393029a8ba6ac6bdf6723
Ashu Jain <ashu12215@gmail.com>
2019-03-29 12:45:03 +0530

    building home section addig background video in home section

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,6 @@
+/*_________preloader___________________*/
 $(window).on('load',function(){
 	$('#status').fadeOut();
-    $('#preloader').delay(1000).fadeOut();
+    $('#preloader').fadeOut();
 });
+/*______________________________________*/
