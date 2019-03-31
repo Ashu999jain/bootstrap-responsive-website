2fe381800f00300e2f67f94d1cd577224e978984
Ashu Jain <ashu12215@gmail.com>
2019-03-31 10:05:03 +0530

    adding owl-carousel jquery slide down effect to our team section

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,21 @@ $(window).on('load',function(){
     $('#preloader').fadeOut();
 });
 /*______________________________________*/
+
+
+$(function(){
+$('#team-members').owlCarousel({
+	items : 2,
+	smartSpeed:700,
+	loop :true,
+	autoplay: true,
+	autoplayHoverPause: true,
+	nav: true,
+	dots:false,
+	navText: ['<i class="fa fa-angle-left"></i>' ,
+	'<i class="fa fa-angle-right"></i>']
+
+
+
+});
+});
